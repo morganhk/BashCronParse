71fb7ec1f13d1b0b7716168288408d96ca5385f4
Morgan Aasdam <morgan.cityu@gmail.com>
2012-05-25 16:24:19 +0800

    first commit

---
+++ cronParse.bash
@@ -0,0 +1,237 @@
+#!/usr/bin/env bash
+####################################################################################################
+#                                          ..,:;riS5X3Gi.  .rSsr;:,.                                          
+#                                     .:rXM@@@@@@@@@@@@M    #@@@@@@@@As:.                                     
+#                                 :SA@@@@@@@@@@@@@@@@@@@.  .@@@@@@@@@@@@@@#2;                                 
+#                             .rH@@@@@@@@@@@@@@@@MA35Shi    sH@@@@@@@@@@@@@@@@@S.                             
+#                          .rM@@@@@@@@@@@H2r:                     .;iG#@@@@@@@@@@@2,                          
+#                        ;A@@@@@@@@@H2;.                                :SA@@@@@@@@@@S.                       
+#                     .2@@@@@@@@@9:                                         .5@@@@@@@@@H:                     
+#                   .X@@@@@@@@h:                                               .S@@@@@@@@#;                   
+#                    .               .rA@@;                          #@Mr.        :A@@@@@@@@;                 
+#                                 :2@@@@@@:                          &@@@@s          2@@@@@@@#:               
+#                              .X@@@@@@@@@:                 ,......  2@@@@S            i@@@@@@@&.             
+#                            :M@@@@@@@@@Gr    ,;2A#@@@@M.  :@@@@@@@@@@@##@@@@@@@@X           .,:.             
+#                          ;@@@@@@@@Mr     .B@@@@@@@@@@@.  .#@@@@@@@@@@##@@@@@@@@@@X                          
+#                        :@@@@@@@@2.   r@@,.@@@@@@@@@@@H             i@@@B;        .                          
+#                      ,&@@@##@@i   r@@@@@  M@@@@&A###@A    2BAGG&:  s@@@#:  ;MAAHM#@@@s                      
+#                     r@@@@@@@S   X@@@@@@#  39;    G@@@&   ,@@@@@@h  5@@@@s  &@@@@@@@@@@B,                    
+#                    2@@@##@&   h@@@@@@@@#.        &@@@A                                ,                     
+#                  ,A@@##@@;  i@@@@###i2@@. .,,,,.rH@@@A          ,:,    .,::.         .,:;;;rr;;rr;;rr;      
+#                 .#@@##@A   @@@@#@@3  ;@@ .@@@@@@@@@@@B   .#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@X     
+#                .@@@##@2  r@@@#@@@@@##@@@..@@@@@@@@#@@H   .#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5    
+#               ,H@@B#@i  X@@@#@B:  .,,...  .         ..    A@@@B:  ..                             ,A@@@@@,   
+#               5@@#@@2  2@@###2           .9&G&AAA9X9AX    A@@@A                                   ;@@@@@B   
+#              s@@#B@A  i@@####3iX&AAA&AM& ,@@@irr9@@@@@.   &@@@#    r#BA&&&G&A&G&&&Hr               i@@@@@:  
+#              B@#M@A  r@@@M#@@@@@@@@@@@@@, @@G:..rA@@@M.   &@@@#.   #@@@@@@@@@@@@@@@@A              .@@#@@A  
+#             5@@##@r  #@#B##99&AHHAAAAH#A  @@@@@@@@M#@M.   &@@@@,   i#MMAAAAAAAAAAAH@M,              5@@@@@: 
+#            .@@@@@A. 5@@##B:              .@@@@@####@@#.   &@@@@,                                    ,@@#@@i.
+#            ;@@#@@r  @@##@X                     r#@@HS,    &@@@@,                                     M@#@@9:
+#                    r@@#M#@@@@@@@@@@@@@@@,    ;#@@A; iX.   A@@@@.                                     X@#@@As
+#                   .G@@M#@@@@@@@@@@@@@@@@: ,3@@@B; ;#@#    G@@@@,                                     r#@@@A2
+#                   ,#@@##5, .....,.....,: :@@@B:.rM@@@A    A@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B#@##@MG
+#                   ,@@@@@r                ;@@r,X@@@@@@#    &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B
+# riXXXXX333XXX3A9,  i2i52:                    r32srr;i&Hh22S;;;sssssssssssssssssssssssssssssssiiiiiisrrrrrs:s
+# H@@@@@@@@@@@@@@@X             ;AAAX,   ;9XiS.        ;@@@@,                                                 
+# 9@@@@@@@@@@@@@@@X        ,r;:r#@@@@A;,;A@@@@B,..     ;@@@@s..............................................,.,
+# 5M@@##@@@@@@@#@@B,       5@@@@@@@@@@@@@@@@@@@@@@@i   ;@@#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@3
+# .               .        r@@@@@###@@@@@@@##@@@@@@s   ;@@#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Hi
+#                               ;#@@H:   ;H@#@r        ;@@@M;                                              . .
+#                    .A@@@@M&932&@###GX39&####AX9HG;   ;@@@M.                                                 
+#                     ;@@@@@@@@@@#M#@@@@@@@###@@@@@2   ;@@@#;                                                 
+#                      :XSssrr&@##@@@XsrSB@@@@@X;iS:   :@@@@r                                                 
+#   rMh2h5,                  ;B@@@@i      :B@@@A,      ;@@@@r                                                 
+#   5@@@@@@,               2@@@@#s          .X@@@@s    :@@@@r          Fake Hong Kong-er
+#    2@@@@@i            .9@@@@@X,  .,:::,,.   r#@@@S   ;@@@@r
+#     #@@@@@r         ,&@@@@@X  ;@@@@@@@@@@@@@: :@@2   ;@@@@r           Morgan Aasdam
+#     .#@@@@@:      :G@@@@@9.   S@@@@@@@@@@@@@r   ;;   :@@@@r              雅文錦"
+#      ;@@@@@@.    :@@@@@h.     i@@@Mr.  i#@@@s        ;@@@@r
+#       r@@@@@@s    .H@M.       s@@@H.   .H@@@i        ;@@@@r          http://morgan.hk
+#        ,rrr2AS.               r@@@#: ;A@@@@@2        :@@@@r
+#                               i@@@@;  :5#@@@X        ;@@@@r
+#                               s@@@@:      :5;        ;@@@@r
+#                               s@@@@2                 ;@@@@r
+#             rB@MBB#A:         i@@@@@@@X:             :@@@@r
+#              X@@@@@@@A,       ;@@@@@@@@@@@Gs,        ;@@@@r
+#                X@@@@@@@A:       ;G@@@@@@@@@@@@@@A:   :@@@@r
+#                  3@@@@@@@#r        ,iM@@@@@@@@@@@S   ;@@@@r
+#                    9@@@@@@@@h,         .r3M@@@@@@i   :@@@@r
+#                      X@@@@@@@@@X,             :i2;   :@@@@r
+#                        r#@@@@@@@@@Bs:                :@@@@r
+#                          .S@@@@@@@@@@@@GS;.          :@@@@r
+#                             .5#@@@@@@@@@@@@@@@@M2,   :@@@@r
+#                                 rh#@@@@@@@@@@@@@@i   :@@@@r
+#                                     :sG#@@@@@@@@@r   :@@@@r
+#                                        .::;sS2hAh;   ;AB#Ar.
+####################################################################################################
+# Simple Bash script that takes a username and a crontab file, parses it and then runs the
+#          Commands that are to be run at that set timeframe
+# 
+# Author: Morgan Aasdam
+# Contact: http://morgan.hk
+# Creation date: 2012-05-25
+# Plaform/Language: FreeBSD 8.2 (tested) / Bash
+# License: Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
+# License info: http://creativecommons.org/licenses/by-nc-sa/3.0/
+#
+# Dependencies:
+#    - bash >=4.1.10
+#    - grep >= 2.5.1
+#
+# Usage: commandName.bash username crontabFile
+#
+# Notes: Only parses the following types of special times: * and */number
+#			eg: 0 or * or */5
+#		 Script originally used because the non-root users' crontabs were not persistant on the system
+#			so root calls the script every minute from its own crontab and runs the commands as the
+#			user on his behalf. Crontab is stored in the user's home folder.
+#        Hard-coded to handle the following tab-separated format:
+#           Min	Hour	Day	Month	DayOfWeek	Commmand
+#		    eg: */5	*	*	*	*	sleep 30
+#
+#
+####################################################################################################
+
+# Got root?
+test "$(whoami)" != 'root' && (echo you are using a non-privileged account; exit 1)
+
+#Go to /tmp
+cd /tmp
+
+#Keep the username on hand for su
+suusername=$1
+
+# Splash if formatting of the command is incorrect
+function splash {
+	echo  -e "                                         ..,:;riS5X3Gi.  .rSsr;:,.\n                                    .:rXM@@@@@@@@@@@@M    #@@@@@@@@As:.\n                                :SA@@@@@@@@@@@@@@@@@@@.  .@@@@@@@@@@@@@@#2;\n                            .rH@@@@@@@@@@@@@@@@MA35Shi    sH@@@@@@@@@@@@@@@@@S.\n                         .rM@@@@@@@@@@@H2r:                     .;iG#@@@@@@@@@@@2,\n                       ;A@@@@@@@@@H2;.                                :SA@@@@@@@@@@S.\n                    .2@@@@@@@@@9:                                         .5@@@@@@@@@H:\n                  .X@@@@@@@@h:                                               .S@@@@@@@@#;\n                   .               .rA@@;                          #@Mr.        :A@@@@@@@@;\n                                :2@@@@@@:                          &@@@@s          2@@@@@@@#:\n                             .X@@@@@@@@@:                 ,......  2@@@@S            i@@@@@@@&.\n                           :M@@@@@@@@@Gr    ,;2A#@@@@M.  :@@@@@@@@@@@##@@@@@@@@X           .,:.\n                         ;@@@@@@@@Mr     .B@@@@@@@@@@@.  .#@@@@@@@@@@##@@@@@@@@@@X\n                       :@@@@@@@@2.   r@@,.@@@@@@@@@@@H             i@@@B;        .\n                     ,&@@@##@@i   r@@@@@  M@@@@&A###@A    2BAGG&:  s@@@#:  ;MAAHM#@@@s\n                    r@@@@@@@S   X@@@@@@#  39;    G@@@&   ,@@@@@@h  5@@@@s  &@@@@@@@@@@B,\n                   2@@@##@&   h@@@@@@@@#.        &@@@A                                ,\n                 ,A@@##@@;  i@@@@###i2@@. .,,,,.rH@@@A          ,:,    .,::.         .,:;;;rr;;rr;;rr;\n                .#@@##@A   @@@@#@@3  ;@@ .@@@@@@@@@@@B   .#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@X\n               .@@@##@2  r@@@#@@@@@##@@@..@@@@@@@@#@@H   .#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5\n              ,H@@B#@i  X@@@#@B:  .,,...  .         ..    A@@@B:  ..                             ,A@@@@@,\n              5@@#@@2  2@@###2           .9&G&AAA9X9AX    A@@@A                                   ;@@@@@B\n             s@@#B@A  i@@####3iX&AAA&AM& ,@@@irr9@@@@@.   &@@@#    r#BA&&&G&A&G&&&Hr               i@@@@@:\n             B@#M@A  r@@@M#@@@@@@@@@@@@@, @@G:..rA@@@M.   &@@@#.   #@@@@@@@@@@@@@@@@A              .@@#@@A\n            5@@##@r  #@#B##99&AHHAAAAH#A  @@@@@@@@M#@M.   &@@@@,   i#MMAAAAAAAAAAAH@M,              5@@@@@:\n           .@@@@@A. 5@@##B:              .@@@@@####@@#.   &@@@@,                                    ,@@#@@i.\n           ;@@#@@r  @@##@X                     r#@@HS,    &@@@@,                                     M@#@@9:\n                   r@@#M#@@@@@@@@@@@@@@@,    ;#@@A; iX.   A@@@@.                                     X@#@@As\n                  .G@@M#@@@@@@@@@@@@@@@@: ,3@@@B; ;#@#    G@@@@,                                     r#@@@A2\n                  ,#@@##5, .....,.....,: :@@@B:.rM@@@A    A@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B#@##@MG\n                  ,@@@@@r                ;@@r,X@@@@@@#    &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B\nriXXXXX333XXX3A9,  i2i52:                    r32srr;i&Hh22S;;;sssssssssssssssssssssssssssssssiiiiiisrrrrrs:s\nH@@@@@@@@@@@@@@@X             ;AAAX,   ;9XiS.        ;@@@@,                                       \n9@@@@@@@@@@@@@@@X        ,r;:r#@@@@A;,;A@@@@B,..     ;@@@@s..............................................,.,\n5M@@##@@@@@@@#@@B,       5@@@@@@@@@@@@@@@@@@@@@@@i   ;@@#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@3\n.               .        r@@@@@###@@@@@@@##@@@@@@s   ;@@#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Hi\n                              ;#@@H:   ;H@#@r        ;@@@M;                                              . .\n                   .A@@@@M&932&@###GX39&####AX9HG;   ;@@@M.\n                    ;@@@@@@@@@@#M#@@@@@@@###@@@@@2   ;@@@#;\n                     :XSssrr&@##@@@XsrSB@@@@@X;iS:   :@@@@r\n  rMh2h5,                  ;B@@@@i      :B@@@A,      ;@@@@r\n  5@@@@@@,               2@@@@#s          .X@@@@s    :@@@@r          Fake Hong Kong-er\n   2@@@@@i            .9@@@@@X,  .,:::,,.   r#@@@S   ;@@@@r\n    #@@@@@r         ,&@@@@@X  ;@@@@@@@@@@@@@: :@@2   ;@@@@r           Morgan Aasdam\n    .#@@@@@:      :G@@@@@9.   S@@@@@@@@@@@@@r   ;;   :@@@@r              雅文錦\n     ;@@@@@@.    :@@@@@h.     i@@@Mr.  i#@@@s        ;@@@@r\n      r@@@@@@s    .H@M.       s@@@H.   .H@@@i        ;@@@@r          http://morgan.hk\n       ,rrr2AS.               r@@@#: ;A@@@@@2        :@@@@r\n                              i@@@@;  :5#@@@X        ;@@@@r\n                              s@@@@:      :5;        ;@@@@r\n                              s@@@@2                 ;@@@@r\n            rB@MBB#A:         i@@@@@@@X:             :@@@@r\n             X@@@@@@@A,       ;@@@@@@@@@@@Gs,        ;@@@@r\n               X@@@@@@@A:       ;G@@@@@@@@@@@@@@A:   :@@@@r\n                 3@@@@@@@#r        ,iM@@@@@@@@@@@S   ;@@@@r\n                   9@@@@@@@@h,         .r3M@@@@@@i   :@@@@r\n                     X@@@@@@@@@X,             :i2;   :@@@@r\n                       r#@@@@@@@@@Bs:                :@@@@r\n                         .S@@@@@@@@@@@@GS;.          :@@@@r\n                            .5#@@@@@@@@@@@@@@@@M2,   :@@@@r\n                                rh#@@@@@@@@@@@@@@i   :@@@@r\n                                    :sG#@@@@@@@@@r   :@@@@r\n                                       .::;sS2hAh;   ;AB#Ar."
+	echo "Simple Crontab Parsing in Bash"
+	echo "======================="
+	echo "Morgan Aasdam, May 2012"
+	echo "======================="
+	echo "Missing Username"
+	echo "Usage: $0 username crontabFile"
+	echo "Hard-coded following tab-separated format: min	hour	day	month	dow	command"
+	echo "Handles only */* and *. Comma separated values not processed"
+	exit
+}
+
+# If there is no username, splash and exit
+if [ -z $suusername ]; then
+	splash
+fi
+
+# If there is no crontab, splash and exit
+if [ -z $2 ]; then
+	splash
+fi
+
+
+#######################################
+# Here starts the meat of the program #
+#######################################
+
+# Read Crontab and find all lines that are valid (eg: 0 9 * */5)
+cat $2 | grep "^[*0-9][	*/0-9]" | while read line
+do
+	# what Tab are we processing?
+	index=0
+	
+	#For each Tab, do the following
+	echo "$line" | tr "\t" "\n" | while read bob
+	do
+		
+		#Minutes
+		if [ "$index" = "0" ]; then
+			#handle * sign
+			if echo "$bob" | grep '^\*/[0-9]\{1,2\}' >/dev/null; then
+				moduloOp=$((`date +%M`%${bob##*/}))
+				if [ moduloOp != "0" ]; then
+					break
+				fi
+			fi
+			#handle numbers
+			if echo "$bob" | grep '^[0-9]\{1,2\}' >/dev/null; then
+				if [ `date +%M` != "$bob" ]; then
+					break
+				fi
+			fi
+		
+		#Hours
+		elif [ "$index" = "1" ]; then
+			#handle * sign
+			if echo "$bob" | grep '^\*/[0-9]\{1,2\}' >/dev/null; then
+				moduloOp=$((`date +%k`%${bob##*/}))
+				if [ moduloOp != "0" ]; then
+					break
+				fi
+			fi
+			#handle numbers
+			if echo "$bob" | grep '^[0-9]\{1,2\}' >/dev/null; then
+				if [ `date +%k` != "$bob" ]; then
+					break
+				fi
+			fi
+		
+		#Day
+		elif [ "$index" = "2" ]; then
+			#handle * sign
+			if echo "$bob" | grep '^\*/[0-9]\{1,2\}' >/dev/null; then
+				moduloOp=$((`date +%d`%${bob##*/}))
+				if [ moduloOp != "0" ]; then
+					break
+				fi
+			fi
+			#handle numbers
+			if echo "$bob" | grep '^[0-9]\{1,2\}' >/dev/null; then
+				if [ `date +%d` != "$bob" ]; then
+					break
+				fi
+			fi
+		
+		#Month
+		elif [ "$index" = "3" ]; then
+			#handle * sign
+			if echo "$bob" | grep '^\*/[0-9]\{1,2\}' >/dev/null; then
+				moduloOp=$((`date +%m`%${bob##*/}))
+				if [ moduloOp != "0" ]; then
+					break
+				fi
+			fi
+			#handle numbers
+			if echo "$bob" | grep '^[0-9]\{1,2\}' >/dev/null; then
+				if [ `date +%m` != "$bob" ]; then
+					break
+				fi
+			fi
+		
+		#Day of Week
+		elif [ "$index" = "4" ]; then
+			#handle * sign
+			if echo "$bob" | grep '^\*/[0-9]\{1,2\}' >/dev/null; then
+				moduloOp=$((`date +%u`%${bob##*/}))
+				if [ moduloOp != "0" ]; then
+					break
+				fi
+			fi
+			#handle numbers
+			if echo "$bob" | grep '^[0-9]\{1,2\}' >/dev/null; then
+				if [ `date +%u` != "$bob" ]; then
+					break
+				fi
+			fi
+		
+		# If we reached this point, then it is the right time to run the command
+		elif [ "$index" = "5" ]; then
+			echo "Command is: $bob"
+			
+			#Run the command using nohup and su in order to run it as the user and let other tasks
+			#  in the crontab to be run
+			nohup echo "$bob" | su "$suusername"  > /dev/null 2>&1 &
+		
+		fi
+		
+		#increment index
+		index=$((index+1))
+		
+	done
+done
